ea17cb6414ebbc45d5e05d3d361665faa3ed0ba4
Clemens Adolphs <clemens.adolphs@gmail.com>
2023-12-05 15:08:28 -0800

    Change gradio version due to slow inference

--- simple_script.py
+++ simple_script.py
@@ -2,85 +2,97 @@
 from dataclasses import dataclass
 import os
 
-from modal import Image, NetworkFileSystem, Stub
+from modal import Image, Stub, method, asgi_app, Secret
 from pathlib import Path
+from fastapi import FastAPI
 
 stub = Stub(name="face-shapes")
+MODEL_DIR = "/face_model"
+BASE_MODEL = "lagerbaer/female_face_shapes"
+
+
+web_app = FastAPI()
+
+
+def download_model_to_folder():
+    from huggingface_hub import snapshot_download
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    snapshot_download(
+        BASE_MODEL,
+        local_dir=MODEL_DIR,
+        token=os.environ["HUGGINGFACE_TOKEN"],
+    )
+
 
 # Image packages roughly taken from the modal docs
 image = (
     Image.debian_slim()
     .pip_install(
         "fastai",
-        "gradio",
+        "gradio~=3.6",
         "httpx",
         # When using pip PyTorch is not automatically installed by fastai.
         "torch",
         "torchvision",
+        "hf-transfer~=0.1",
     )
-    .run_commands("mkdir /faces")
-    .copy_local_dir("faces", "/faces")
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+    .run_function(
+        download_model_to_folder,
+        secret=Secret.from_name("my-huggingface-secret"),
+    )
 )
 
-# Now we need a volume to store the model artifact
-volume = NetworkFileSystem.persisted("faceshape-training-vol")
-
-# Some constants for the environment
-FASTAI_HOME = "/fastai_home"
-MODEL_CACHE = Path(FASTAI_HOME, "models")
 USE_GPU = os.environ.get("MODAL_GPU")
-MODEL_EXPORT_PATH = Path(MODEL_CACHE, "model-exports", "inference.pkl")
-
-# Apparently we need this so the fastai library behaves nicely, i.e., saves data into the path of our
-# persistent volume
-os.environ["FASTAI_HOME"] = FASTAI_HOME
 
 
-@dataclass
-class Config:
-    epochs: int = 10
-    dim: int = 256
-    gpu: str = USE_GPU
+@stub.cls(image=image, gpu=USE_GPU)
+class FaceClassifier:
+    def __enter__(self):
+        from fastai.learner import load_learner
+
+        print("Loading the learner...")
+        self.learner = load_learner(Path(MODEL_DIR, "learner.pkl"))
+        print("Done")
+
+    @method()
+    def predict(self, image) -> str:
+        print("Predicting label")
+        prediction = self.learner.predict(image)
+        classification = prediction[0]
+        return classification
 
 
-@stub.function(
-    image=image,
-    gpu=USE_GPU,
-    network_file_systems={str(MODEL_CACHE): volume},
-    timeout=2700,
-)
-def train():
-    from fastai.data.transforms import parent_label
-    from fastai.metrics import accuracy
-    from fastai.vision.all import Resize, models, vision_learner, RatioResize
-    from fastai.vision.data import (
-        CategoryBlock,
-        DataBlock,
-        ImageBlock,
-        TensorCategory,
-        get_image_files,
-        RandomSplitter,
+@stub.function(image=image, gpu=USE_GPU)
+def classify_url(image_url: str) -> None:
+    """Utility function for command-line classification runs."""
+    import httpx
+
+    r = httpx.get(image_url)
+    if r.status_code != 200:
+        raise RuntimeError(f"Could not download '{image_url}'")
+
+    classifier = FaceClassifier()
+    label = classifier.predict.remote(image=r.content)
+    print(f"Classification: {label}")
+
+
+@stub.function(image=image)
+@asgi_app()
+def fastapi_app():
+    import gradio as gr
+    from gradio.routes import mount_gradio_app
+
+    classifier = FaceClassifier()
+    interface = gr.Interface(
+        fn=classifier.predict.remote,
+        inputs=gr.Image(shape=(224, 224)),
+        outputs="label",
     )
-    from fastai.vision.augment import CropPad
-
-    config: Config = Config()
-
-    dataset_path = Path("/", "faces")
-
-    # Let's see if we need to tweak the resize method!
-    dblock = DataBlock(
-        blocks=[ImageBlock(), CategoryBlock()],
-        get_items=get_image_files,
-        splitter=RandomSplitter(valid_pct=0.2, seed=42),
-        get_y=parent_label,
-        item_tfms=[RatioResize(config.dim), CropPad(config.dim)],
+    return mount_gradio_app(
+        app=web_app,
+        blocks=interface,
+        path="/",
     )
-
-    dls = dblock.dataloaders(dataset_path, bs=64)
-
-    learn = vision_learner(dls, models.resnet18, metrics=accuracy)
-    learn.fine_tune(config.epochs, freeze_epochs=3)
-    learn.save("faces_model")
-
-    MODEL_EXPORT_PATH.parent.mkdir(exist_ok=True)
-    learn.export(MODEL_EXPORT_PATH)
